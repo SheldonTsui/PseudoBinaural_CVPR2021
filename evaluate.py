87f7be08d41748995c561ad1dc7bda453d4ee553
SheldonTsui <forrest.tusi@gmail.com>
2021-07-08 13:17:24 +0800

    update evaluation metrics

--- evaluate.py
+++ evaluate.py
@@ -26,29 +26,6 @@ def get_content(value_list, text):
 def normalize(samples):
     return samples / np.maximum(1e-20, np.max(np.abs(samples)))
 
-def STFT_L1_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2):
-    #channel1
-    real = np.expand_dims(np.real(predicted_spect_channel1), axis=0)
-    imag = np.expand_dims(np.imag(predicted_spect_channel1), axis=0)
-    predicted_realimag_channel1 = np.concatenate((real, imag), axis=0)
-    real = np.expand_dims(np.real(gt_spect_channel1), axis=0)
-    imag = np.expand_dims(np.imag(gt_spect_channel1), axis=0)
-    gt_realimag_channel1 = np.concatenate((real, imag), axis=0)
-    channel1_distance = np.mean(np.abs(predicted_realimag_channel1 - gt_realimag_channel1))
-
-    #channel2
-    real = np.expand_dims(np.real(predicted_spect_channel2), axis=0)
-    imag = np.expand_dims(np.imag(predicted_spect_channel2), axis=0)
-    predicted_realimag_channel2 = np.concatenate((real, imag), axis=0)
-    real = np.expand_dims(np.real(gt_spect_channel2), axis=0)
-    imag = np.expand_dims(np.imag(gt_spect_channel2), axis=0)
-    gt_realimag_channel2 = np.concatenate((real, imag), axis=0)
-    channel2_distance = np.mean(np.abs(predicted_realimag_channel2 - gt_realimag_channel2))
-
-    #sum the distance between two channels
-    stft_l1_distance = channel1_distance + channel2_distance
-    return float(stft_l1_distance)
-
 def STFT_L2_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2):
     #channel1
     real = np.expand_dims(np.real(predicted_spect_channel1), axis=0)
@@ -87,74 +64,31 @@ def Envelope_distance(predicted_binaural, gt_binaural):
     envelope_distance = channel1_distance + channel2_distance
     return float(envelope_distance)
 
-def MSE_distance(predicted_binaural, gt_binaural):
+def SNR(predicted_binaural, gt_binaural):
     mse_distance = np.mean(np.power((predicted_binaural - gt_binaural), 2))
     snr = 10. * np.log10((np.mean(gt_binaural**2) + 1e-4) / (mse_distance + 1e-4))
 
-    return float(mse_distance), float(snr)
+    return float(snr)
 
-def STFT_MSE_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2):
+def Magnitude_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2):
     stft_mse1 = np.mean(np.power(np.abs(predicted_spect_channel1 - gt_spect_channel1), 2))
     stft_mse2 = np.mean(np.power(np.abs(predicted_spect_channel2 - gt_spect_channel2), 2))
 
     return float(stft_mse1 + stft_mse2)
 
-def Mag_Phase_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2):
-    #channel1
-    pred_mag, pred_phase = librosa.magphase(predicted_spect_channel1)
-    gt_mag, gt_phase = librosa.magphase(gt_spect_channel1)
-    pred_mag = librosa.amplitude_to_db(pred_mag)
-    gt_mag = librosa.amplitude_to_db(gt_mag)
-    pred_angle = np.angle(pred_phase) 
-    gt_angle = np.angle(gt_phase) 
-    mag_distance1 = np.sqrt(np.mean(np.power((pred_mag - gt_mag), 2)))
-    angle_init_distance1 = np.abs(pred_angle - gt_angle)
-    angle_distance1 = np.mean(np.minimum(angle_init_distance1, np.clip(2 * pi - angle_init_distance1, a_min=0, a_max=2*pi))) 
-
-    #channel2
-    pred_mag, pred_phase = librosa.magphase(predicted_spect_channel2)
-    gt_mag, gt_phase = librosa.magphase(gt_spect_channel2)
-    pred_mag = librosa.amplitude_to_db(pred_mag)
-    gt_mag = librosa.amplitude_to_db(gt_mag)
-    pred_angle = np.angle(pred_phase) 
-    gt_angle = np.angle(gt_phase) 
-    mag_distance2 = np.sqrt(np.mean(np.power((pred_mag - gt_mag), 2)))
-    angle_init_distance2 = np.abs(pred_angle - gt_angle)
-    angle_distance2 = np.mean(np.minimum(angle_init_distance2, np.clip(2 * pi - angle_init_distance2, a_min=0, a_max=2*pi))) 
-
-    #sum the distance between two channels
-    mag_distance = mag_distance1 + mag_distance2 
-    angle_distance = angle_distance1 + angle_distance2 
-    return float(mag_distance), float(angle_distance) 
-
-def Mag_diff_distance(predicted_binaural, gt_binaural):
+def Angle_Diff_distance(predicted_binaural, gt_binaural):
     gt_diff = gt_binaural[0] - gt_binaural[1]
     pred_diff = predicted_binaural[0] - predicted_binaural[1]
     gt_diff_spec = librosa.core.stft(gt_diff, n_fft=512, hop_length=160, win_length=400, center=True)
     pred_diff_spec = librosa.core.stft(pred_diff, n_fft=512, hop_length=160, win_length=400, center=True)
-    pred_diff_mag, pred_diff_phase = librosa.magphase(pred_diff_spec)
-    gt_diff_mag, gt_diff_phase = librosa.magphase(gt_diff_spec)
-    pred_diff_mag = librosa.amplitude_to_db(pred_diff_mag)
-    gt_diff_mag = librosa.amplitude_to_db(gt_diff_mag)
+    _, pred_diff_phase = librosa.magphase(pred_diff_spec)
+    _, gt_diff_phase = librosa.magphase(gt_diff_spec)
     pred_diff_angle = np.angle(pred_diff_phase)
     gt_diff_angle = np.angle(gt_diff_phase)
-    mag_diff_distance = np.sqrt(np.mean(np.power((pred_diff_mag - gt_diff_mag), 2)))
     angle_diff_init_distance = np.abs(pred_diff_angle - gt_diff_angle)
     angle_diff_distance = np.mean(np.minimum(angle_diff_init_distance, np.clip(2 * pi - angle_diff_init_distance, a_min=0, a_max=2*pi))) 
-    #pred_diff_spec = np.stack((np.real(pred_diff_spec), np.imag(pred_diff_spec)))
-    #gt_diff_spec = np.stack((np.real(gt_diff_spec), np.imag(gt_diff_spec)))
     
-    return float(mag_diff_distance), float(angle_diff_distance)
-
-def Envelope_diff_distance(predicted_binaural, gt_binaural):
-    gt_diff = gt_binaural[0] - gt_binaural[1]
-    pred_diff = predicted_binaural[0] - predicted_binaural[1]
-
-    pred_diff = np.abs(hilbert(pred_diff))
-    gt_diff = np.abs(hilbert(gt_diff))
-
-    envelope_diff_distance = np.sqrt(np.mean((pred_diff - gt_diff)**2))
-    return float(envelope_diff_distance)
+    return float(angle_diff_distance)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -162,19 +96,13 @@ def main():
     parser.add_argument('--audio_sampling_rate', default=16000, type=int, help='audio sampling rate')
     parser.add_argument('--real_mono', default=False, type=bool, help='whether the input predicted binaural audio is mono audio')
     parser.add_argument('--normalization', default=True, type=bool)
-    parser.add_argument('--get_detail', action='store_true', help='whether store details')
     parser.add_argument('--audioNames_file', type=str, default='', help='audioNames file')
     args = parser.parse_args()
     stft_distance_list = []
     envelope_distance_list = []
-    mag_diff_distance_list = []
-    envelope_diff_distance_list = []
-    mag_distance_list = []
     angle_distance_list = []
-    mse_distance_list = []
     snr_list = []
-    stft_mse_distance_list = []
-    l1_distance_list = []
+    magnitude_distance_list = []
 
     if len(args.audioNames_file) > 0:
         audioNames = mmcv.list_from_file(args.audioNames_file)
@@ -212,59 +140,35 @@ def main():
 
         #get results for this audio
         stft_distance_list.append(STFT_L2_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2))
-        l1_distance_list.append(STFT_L1_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2))
-        #mag_diff_distance_list.append(Mag_diff_distance(predicted_binaural, gt_binaural))
-        mag_diff_distance, angle_diff_distance = Mag_diff_distance(predicted_binaural, gt_binaural)
-        mag_diff_distance_list.append(mag_diff_distance)
         envelope_distance_list.append(Envelope_distance(predicted_binaural, gt_binaural))
-        envelope_diff_distance_list.append(angle_diff_distance)
-        #cur_mag_dist, cur_angle_dist = Mag_Phase_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2)
-        #mag_distance_list.append(cur_mag_dist)
-        #angle_distance_list.append(cur_angle_dist)
-        cur_mse_dis, cur_snr = MSE_distance(predicted_binaural, gt_binaural)
-        mse_distance_list.append(cur_mse_dis)
+        cur_angle_dist = Angle_Diff_distance(predicted_binaural, gt_binaural)
+        angle_distance_list.append(cur_angle_dist)
+        cur_snr = SNR(predicted_binaural, gt_binaural)
         snr_list.append(cur_snr)
-        stft_mse_distance_list.append(STFT_MSE_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2))
+        magnitude_distance_list.append(Magnitude_distance(predicted_spect_channel1, gt_spect_channel1, predicted_spect_channel2, gt_spect_channel2))
         index = index + 1
 
     #print the results
     stft_res = get_content(value_list=stft_distance_list, text='STFT L2 Distance')
-    l1_res = get_content(value_list=l1_distance_list, text='L1 Distance')
     env_res = get_content(envelope_distance_list, text='Average Envelope Distance')
-    mag_diff_res = get_content(mag_diff_distance_list, text='Mag diff Distance')
-    env_diff_res = get_content(envelope_diff_distance_list, text='Average Envelope diff Distance')
-    #mag_res = get_content(mag_distance_list, text='Mag Distance')
-    #angle_res = get_content(angle_distance_list, text='Angle Distance')
-    mse_res = get_content(mse_distance_list, text='MSE Distance')
-    snr_res = get_content(snr_list, text='Snr')
-    stft_mse_res = get_content(stft_mse_distance_list, text='STFT MSE Distance')
+    magnitude_res = get_content(magnitude_distance_list, text='Magnitude Distance')
+    angle_res = get_content(angle_distance_list, text='Phase Distance')
+    snr_res = get_content(snr_list, text='SNR')
 
     print(stft_res)
-    print(l1_res)
     print(env_res)
-    print(mse_res)
-    print(stft_mse_res)
-    print(mag_diff_res)
-    print(env_diff_res)
-    #print(mag_res)
-    #print(angle_res)
+    print(magnitude_res)
+    print(angle_res)
     print(snr_res)
 
     store_content = [args.results_root]
     store_content.append(stft_res.split(',')[0])
     store_content.append(env_res.split(',')[0])
-    store_content.append(mse_res.split(',')[0])
-    store_content.append(stft_mse_res.split(',')[0])
-    store_content.append(mag_diff_res.split(',')[0])
-    store_content.append(env_diff_res.split(',')[0])
+    store_content.append(magnitude_res.split(',')[0])
+    store_content.append(angle_res.split(',')[0])
     store_content.append(snr_res.split(',')[0])
-    store_content.append(l1_res.split(',')[0])
     with open('output/six_metrics.txt', 'a') as cur_file:
         cur_file.writelines('\n'.join(store_content) + '\n\n')
-
-    if args.get_detail:
-        detail_metrics = {'stft': stft_distance_list, 'env': envelope_distance_list, 'snr': snr_list} 
-        mmcv.dump(detail_metrics, '{}.pkl'.format(args.results_root))
 
 if __name__ == '__main__':
     main()
